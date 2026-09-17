cdf05052ad792dde613f5aa4326f733cc6ce2460
Cugutayy <s.cagatay.sonmez@gmail.com>
2026-09-18 01:52:38 +0300

    Remove premature prediction proxy

--- web/app/api/prediction/route.ts
+++
@@ -1,7 +0,0 @@
-import { pickSearchParams, proxyJson } from "@/lib/backend";
-
-export const dynamic = "force-dynamic";
-
-export async function GET(request: Request) {
-  return proxyJson("/v1/prediction", pickSearchParams(request, ["total_laps", "samples"]));
-}
